6039473d0d847b9a504a9fbeac32f166c5c1c12b
Ramilsons <ramilsonfelixsilva9@gmail.com>
2025-12-13 14:27:46 -0300

    feat(data-missing-treatment): trasnforming null data to median

---
+++ 3-data-missing-exercise.py
@@ -0,0 +1,15 @@
+import pandas as pd;
+
+pd.set_option('display.max_columns', 42); 
+pd.set_option('display.max_rows', None);
+
+file = pd.read_csv('././data-sets/2015-building-energy-benchmarking.csv');
+
+print('**************** BEFORE TREATMENT **********************');
+print(file['ENERGYSTARScore']);
+
+# Changing the "NaN" (null - missing) values to median of this columns (usigin the other rows);
+file['ENERGYSTARScore'] = file['ENERGYSTARScore'].fillna(file['ENERGYSTARScore'].median());
+print('**************** AFTER TREATMENT **********************');
+print(file['ENERGYSTARScore']);
+
