3219e1bcfbfb7368a0003c71331cd55c5e887f80
Ramilsons <ramilsonfelixsilva9@gmail.com>
2025-12-13 14:36:56 -0300

    feat(data-missing-treatment): show percent of data missing on each column

--- 3-data-missing-exercise.py
+++ 3-data-missing-exercise.py
@@ -5,11 +5,15 @@ pd.set_option('display.max_rows', None);
 
 file = pd.read_csv('././data-sets/2015-building-energy-benchmarking.csv');
 
+percentOfDataMissingEachColumn = (file.isnull().sum() / file.shape[0]) * 100;
+print(percentOfDataMissingEachColumn);
+
+
 print('**************** BEFORE TREATMENT **********************');
-print(file['ENERGYSTARScore']);
+# print(file['ENERGYSTARScore']);
 
 # Changing the "NaN" (null - missing) values to median of this columns (usigin the other rows);
 file['ENERGYSTARScore'] = file['ENERGYSTARScore'].fillna(file['ENERGYSTARScore'].median());
 print('**************** AFTER TREATMENT **********************');
-print(file['ENERGYSTARScore']);
+# print(file['ENERGYSTARScore']);
 
