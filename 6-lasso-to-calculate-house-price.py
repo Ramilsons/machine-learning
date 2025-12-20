d23365227470818a193ed142bd9f00be4f50e9f3
Ramilsons <ramilsonfelixsilva9@gmail.com>
2025-12-20 14:34:59 -0300

    feat(regularization-methods): explanation notes

---
+++ 6-lasso-to-calculate-house-price.py
@@ -0,0 +1,8 @@
+# Lasso is a function that use L1 Method of regularization. The ridge regression (previous lesson) apply the L2 Method of regularization.
+# The regularization is used to give lower weight avoiding distortions
+
+# Resume
+# Linear Regression - It doesn't use any method of regularization
+# Linear Regression + L2 Method Regularization = Ridge Regression
+# Linear Regression + L1 Method Regulazization = Lasso
+
